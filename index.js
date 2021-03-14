7e32bb1aad6c3ac80e614b0082613f05deadebe5
JaredFitz <jaredjfitzpatrick@gmail.com>
2021-03-14 09:47:43 -0700

    test with fitz

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const client = new Discord.Client();
 
 client.on("message", function(message) {
-  if (message.author.username === 'Mooki') {
+  if (message.author.username === 'Fitzalici0us') {
     if (message.content.toLowerCase().includes('big') || message.content.toLowerCase().includes('scary')) {
       message.channel.send('uwu');
     }
