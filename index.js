19d2bf53cb3726b836fed038e367fdaf14cf83a8
Jared Fitzpatrick <jaredjfitzpatrick@gmail.com>
2021-03-17 19:44:03 -0700

    user env for username

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const Discord = require("discord.js");
 require('dotenv').config();
 
+const botToken = process.env.BOT_TOKEN;
+const userName = process.env.USER_NAME;
+
 const client = new Discord.Client();
 
+console.log('Client Started');
+
 client.on("message", function(message) {
-  if (message.author.username === 'Fitzalici0us') {
+  if (message.author.username === userName) {
     if (message.content.toLowerCase().includes('big') || message.content.toLowerCase().includes('scary')) {
       message.channel.send('uwu');
     }
   }
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(botToken);
